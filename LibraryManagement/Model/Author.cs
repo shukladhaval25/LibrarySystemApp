9e4e41a8ab29db5dafc5afbcf2ff5ba234ce816a
shukla <shukl@DESKTOP-FT786DU>
2021-06-17 10:03:06 +0530

    Add project to git hug

---
+++ LibraryManagement/Model/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Model
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
---
+++ LibraryManagement/Model/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Model
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
